27c55207532f113e3f33a8fb31f2586daedddab6
Pavel Lunin <plunin@gmail.com>
2021-09-27 11:21:01 +0000

    encoding

--- get_votes.py
+++ get_votes.py
@@ -50,7 +50,7 @@ with psycopg.connect("dbname=" + DB_NAME + " user=" + DB_USER) as psql_conn:
         # So the 0th element is of interest
         ballots_conf = ballots[0]["ballots_config"]
 
-        with open(BALLOTS_CONF_FILENAME, 'w', "utf-8") as json_file:
+        with open(BALLOTS_CONF_FILENAME, 'w', encoding="utf-8") as json_file:
             json.dump(ballots_conf, json_file)
 
         # Iterate through districts
